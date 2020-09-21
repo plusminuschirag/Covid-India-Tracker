a3755ed8daa78fa0371aacc41e6f5142afb1c6f7
iamchiragsharma <chiragsharmagtbit@outlook.com>
2020-09-21 09:50:05 +0530

    Changed App Name

--- lib/widgets/chart_state.dart
+++ lib/widgets/chart_state.dart
@@ -1,7 +1,9 @@
-import 'package:covid_chart_app/charts/daily_log_cases.dart';
 import 'package:flutter/material.dart';
+
 import '../charts/daily_new_cases.dart';
 import '../charts/daily_total_cases.dart';
+import '../charts/daily_log_cases.dart';
+
 import '../models/covid_day.dart';
 
 class ChartState extends StatelessWidget {
